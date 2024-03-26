db86ec5f0e3d4611f21d89147766e55ff20e319d
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-26 01:46:33 -0400

    Update Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int answer,guess;
+
+       final int MAX=5;
+        Scanner input=new Scanner(System.in);
+        Random rand = new Random();
+
+        answer = rand.nextInt(MAX)+1;
+
+        System.out.println("Guess a number between 1 and 5");
+
+        guess=input.nextInt();
+
+
+        if (guess==answer)
+        {
+
+            System.out.println("Good job,the nu,mber was"+answer);
+        }else
+        {
+
+            System.out.println("Sorry, but the number was"+answer);
+        }
+
+
     }
 }
